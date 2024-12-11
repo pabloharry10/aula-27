d391b20c7570d9ea49ef543caafd8e02453b9809
pabloharry10 <pabro.souzalima@aluno.senai.br>
2024-12-11 09:58:52 -0500

    função 2.0

--- funcao.py
+++ funcao.py
@@ -1,12 +1,12 @@
-def bom_dia():
-    print("BOM DIA!")
+def horario(hora_do_dia):
+    if (hora_do_dia>=0)and(hora_do_dia <=12):
+        print("Bom dia!")
+    elif (hora_do_dia>=13)and(hora_do_dia<=18):
+        print("Boa tarde!")
+    elif (hora_do_dia>=19)and(hora_do_dia<=23):
+        print("Boa noite!")
+    else:
+        print("esse horario não existe")
 
-def boa_noite():
-    print("\nBOA NOTE!")
-
-def boa_tarde():
-    print("\nBOA TARDE!")
-   
-bom_dia()
-boa_tarde()
-boa_noite()
+resposta = float(input("digite que horas são: "))
+horario(resposta)
