f2360bb5852dfb2e74e986ce9fae07fb7689707b
pabloharry10 <pabro.souzalima@aluno.senai.br>
2024-12-11 11:00:54 -0500

    função 3.0

--- BANCO.py
+++ BANCO.py
@@ -1,6 +1,10 @@
 #cadastro do usuario e senha
 #menu principal
 #ler a escolha do usuario 
+def validar_senha():
+    senha_validar=input("Digite sua senha: ")
+    if senha_validar==senha:
+        return True
 saldo=0.0
 while True:
     escolha_menu= int(input("Escolha uma das opçoes:\n 1.cadastrar \n 2.login \n 3.encerar\n escolha uma dessas opçoes: "))#le a escolha do usuario
@@ -30,27 +34,25 @@ while True:
 
                 elif escolha_principal ==2:
                     valor_saque=float (input("Digite o valor do saque: "))
-                    senha_saque= input("Digite sua senha: ")
 
-                    if senha_saque==senha:
+                    if validar_senha():
                         saldo=saldo-valor_saque
+                        print(f"você sacou: {saldo}")
 
                     else:
                         print("Senha incorreta")
 
                 elif escolha_principal ==3:
                     valor_pix= float (input("Digite o valor do pix: "))
-                    senha_pix= input("Digite sua senha: ")
 
-                    if senha_pix==senha:
+                    if validar_senha():
                         saldo=saldo-valor_pix
 
                     else:
                         print("A sua senha está incorreta")
                 
                 elif escolha_principal==4:
-                    senha_extrato= input("digite sua senha")
-                    if senha_extrato==senha:
+                    if validar_senha():
                         print (f"extrato:{saldo}")
                     
                     else:
@@ -63,4 +65,3 @@ while True:
                         print("senha incorreta")
         else:
             print ("Usuario ou senha incorretos")
-            
